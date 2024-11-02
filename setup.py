bf8a1064305c9e42600276e113c9e2a250696372
Achini Sanjula <achini.sna@gamil.com>
2024-11-03 02:22:06 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='ML',
     version='0.0.1',
-    author='Krish',
+    author='Achini Sanjula',
     author_email='achini.sna@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
