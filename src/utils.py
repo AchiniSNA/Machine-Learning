3eb251a79b1259d0d526cf4c07fb53ad125faf07
AchiniSNA <achini.20@cse.mrt.ac.lk>
2024-12-04 01:22:32 +0530

    predict pipeline added

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
 
 from src.exception import CustomException
@@ -34,5 +35,14 @@ def evaluate_models(X_train, y_train, X_test, y_test,models):
 
         return report
     
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
